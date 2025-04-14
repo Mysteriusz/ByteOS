b3025d113d7418ceb5019aeac22fc651ad5498c4
Revenant <wixxxiw730@gmail.com>
2025-04-14 09:12:05 +0200

    Extended x86_64 specific command list

--- kernel/arch/x86.h
+++ kernel/arch/x86.h
@@ -2,9 +2,13 @@
 
 #include "byteos.h"
 
-#define X86_CACHE_LINE_SIZE 0x40;
+#define X86_CACHE_LINE_SIZE 0x40
 
-extern VOID CLFLUSH(VOID *address);
+extern VOID CLFLUSH(VOID *address); // Invalidates and writes all the CPU cache back to memory
+extern VOID CLFLUSHOPT(VOID *address); // Invalidates and writes all the CPU cache back to memory without serialization
+extern VOID LFENCE(VOID); // Serializes all the memory READ operation called before
+extern VOID SFENCE(VOID); // Serializes all the memory WRITE operation called before
+extern VOID MFENCE(VOID); // Serializes all the memory operation called before
 
-BT_STATUS X86_FlushLine(IN PHYSICAL_ADDRESS address); // Flushes 64 bytes of memory from address into cache
-BT_STATUS X86_FlushPage(IN PHYSICAL_ADDRESS address); // Flushes page size of memory from address into cache
+BT_STATUS X86_FlushLine(IN PHYSICAL_ADDRESS address); // Flushes 64 bytes from CPU cache to memory address
+BT_STATUS X86_FlushPage(IN PHYSICAL_ADDRESS address); // Flushes page size from CPU cache to memory address
--- kernel/arch/x86.s
+++ kernel/arch/x86.s
@@ -1,9 +1,26 @@
 .section .text
+
 .global CLFLUSH
 CLFLUSH:
     movq %rdi, %rax
     clflush (%rax)
-    mfence
+    ret
+
+.global CLFLUSHOPT
+CLFLUSHOPT:
+    movq %rdi, %rax
+    clflushopt (%rax)
+    ret
+
+.global LFENCE
+LFENCE:
+    lfence
+    ret
+.global SFENCE
+SFENCE:
     sfence
+    ret
+.global MFENCE
+MFENCE:
     mfence
     ret
